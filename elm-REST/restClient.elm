edeb53f9848c545f4b5e7ca870a42ac2ce30beea
petcat <catanapetru@gmail.com>
2018-03-17 12:18:18 +0100

    tried my best

--- elm-REST/restClient.elm
+++ elm-REST/restClient.elm
@@ -1,1 +1,52 @@
-//toDo
+import Html exposing (..)
+import Html.App as App
+import Html.Events exposing (..)
+import WebSocket
+
+main : Program Never
+main =
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+type alias Model = List String
+
+type Msg
+  = Send
+  | Receive String
+
+
+init : (Model, Cmd Msg)
+init =
+  (["Welcome!"], Cmd.none)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Send ->
+      (model, WebSocket.send "ws://localhost:1234/hello" "Hello, server!")
+
+    Receive message ->
+      ((List.append model [message]), Cmd.none)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  WebSocket.listen "ws://localhost:1234/hello" Receive
+
+
+view : Model -> Html Msg
+view model =
+  let
+    renderMessage msg =
+      div [] [ text msg ]
+  in
+    div []
+      [ div [] (List.map renderMessage model)
+      , button [onClick Send] [text "Send message to server!"]
+      ]
---
+++ elm-REST/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var expressWS = require('express-ws')(app);
+
+var portNumber = 1234;
+
+
+app.listen(portNumber, function() {
+    console.log("server running on port: " + portNumber);
+})
+
+app.ws('/hello', function(websocket, request) {
+    console.log('A client connected!');
+  
+    websocket.on('message', function(message) {
+      console.log(`A client sent a message: ${message}`);
+      websocket.send('Hello, world!');
+    });
+  });
